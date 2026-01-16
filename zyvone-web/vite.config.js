621318447a3f7763624c560bddb0703b3e3274f8
mohan.napeans <mohan.napeans@gmail.com>
2026-01-16 19:50:38 +0530

    sad

--- zyvone-web/vite.config.js
+++ zyvone-web/vite.config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+});
